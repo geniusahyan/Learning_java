0997db14eba53b2e471f68c5e3625c03d5042ad9
Aslam Ahyan <aslamalam078523@gmail.com>
2023-12-14 20:27:46 +0530

    adding java file

--- ShortedSet.java
+++ ShortedSet.java
@@ -1,8 +1,15 @@
 import java.util.*;
 
-class Student{
+// class Student{
+class Student implements Comparable {
     int rollNo;
     String name;
+
+    public int compareTo(Object O){
+        Student s = (Student) O;
+        return this.rollNo - s.rollNo;
+        // return name.compareTo(s.name) ;
+    }
 
     public Student(int r, String n){
         this.rollNo = r;
@@ -15,6 +22,21 @@ class Student{
 
 class SortedSet{
     public static void main(String[] args){
+
+        TreeSet<Student> college = new TreeSet<Student>();
+        Student s1 = new Student(12,"some");
+        Student s2 = new Student(13,"none");
+        Student s3 = new Student(14,"anot");
+        Student s4 = new Student(15,"funn");
+
+        college.add(s1);
+        college.add(s2);
+        college.add(s3);
+        college.add(s4);
+
+        System.out.println(college);
+
+        /*
         TreeSet<Integer> ts1 = new TreeSet<Integer>();
         ts1.add(40);
         ts1.add(60);
@@ -27,6 +49,16 @@ class SortedSet{
         System.out.println("After "+ts1);
         ts1.add(50);
         System.out.println("After "+ts1);
+
+        TreeSet<Integer> header = (TreeSet<Integer>) ts1.headSet(45);
+        System.out.println("Printing Header");
+        System.out.println(header);
+        TreeSet<Integer> trailer = (TreeSet<Integer>) ts1.tailSet(45);
+        System.out.println("Printing trailer");
+        System.out.println(trailer);
+        System.out.println(ts1.first());
+        System.out.println(ts1.last());
+        */
         
     }
 }
