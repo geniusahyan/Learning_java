5cf133b6fb4127570f9acfd88842e5d4551e12d4
Aslam Ahyan <aslamalam078523@gmail.com>
2023-12-16 22:18:49 +0530

    adding java file

---
+++ Regular_expression/RegexD2.java
@@ -0,0 +1,25 @@
+package Regular_expression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class RegexD2{
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("LV");
+        Matcher matcher = pattern.matcher("LV stand LV for LearnVern");
+        boolean isLookingAt = matcher.lookingAt();
+        // boolean isFind = matcher.find();
+        boolean isFind = matcher.find(12);
+        if (isLookingAt){
+            System.out.println("Matched");
+        }else{
+            System.out.println("Not Matched");
+        }
+
+        if (isFind){
+            System.out.println("Matched");
+        }else{
+            System.out.println("Not Matched");
+        }
+    }
+}
